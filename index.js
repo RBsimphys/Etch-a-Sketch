ebb5d69a4dc11bdc16a9a3bd7a5d00d8e5ac0533
Rami Barakat <Barakat.Rami@outlook.com>
2022-09-17 16:58:39 -0500

    completed grayifying function

--- index.js
+++ index.js
@@ -3,29 +3,27 @@ let canvas = document.querySelector('#canvas');
 let randomColor = document.querySelector('#randomColors');
 let reset = document.querySelector('#reset');
 let erase = document.querySelector('#erase');
+let gray = document.querySelector('#gray');
 let Xinput = document.querySelector('#Xrange');
-let Yinput = document.querySelector('#Yrange');
-
+let colorPicker = document.querySelector('#colorpicker-control')
+let gridoutput = document.querySelector('#output');
 // flags for event listners 
 let randomC = 0;
 let earserSelected = 0;
+let graySelected = 0;
 // create grid 
-
-let Y = Yinput.value;
+let canvasWidth = canvas.offsetWidth;
+let canvasHeight = canvas.offsetHeight;
 let X = Xinput.value;
-createSquares(X, Y);
+createSquares(X);
+
 Xinput.oninput = function () {
     X = Xinput.value;
-    createSquares(X, Y);
-    console.log(`x:${X}\ny:${Y}\n`); 
+    createSquares(X);
+
+    gridoutput.textContent = `${X} x ${X}`;
 };
 
-
-Yinput.oninput = function () {
-    Y = Yinput.value;
-    createSquares(X, Y);
-    console.log(`x:${X}\ny:${Y}\n`);
-};
 
 // color black by default 
 canvas.addEventListener('mousedown', (e) => {
@@ -33,35 +31,69 @@ canvas.addEventListener('mousedown', (e) => {
     e.stopPropagation();
 });
 
-randomColor.addEventListener('mousedown', (e) => {
+
+
+// random colors
+randomColor.addEventListener('click', (e) => {
     if (randomC) {
         earserSelected = 0;
+        graySelected = 0;
         randomC--;
+        gray.style.backgroundColor = 'white';
         randomColor.style.backgroundColor = 'white';
         erase.style.backgroundColor = 'white';
     } else {
         earserSelected = 0;
+        graySelected = 0;
         randomC++;
+        gray.style.backgroundColor = 'white';
         randomColor.style.backgroundColor = 'lightgreen';
         erase.style.backgroundColor = 'white';
     }
 });
 
 
+// // gray colors
+gray.addEventListener('click', (e) => {
+    if (graySelected) {
+        randomC = 0;
+        earserSelected = 0;
+        graySelected--;
+        randomColor.style.backgroundColor = 'white';
+        erase.style.backgroundColor = 'white';
+        gray.style.backgroundColor = 'white';
+    } else {
+        randomC = 0;
+        earserSelected = 0;
+        graySelected++;
+        randomColor.style.backgroundColor = 'white';
+        erase.style.backgroundColor = 'white';
+        gray.style.backgroundColor = 'lightblue';
+    }
+})
+
+
+// erase colors
 erase.addEventListener('click', (e) => {
     if (earserSelected) {
         randomC = 0;
+        graySelected = 0;
         earserSelected--;
-        radrandomColor.style.backgroundColor = 'white';
+        randomColor.style.backgroundColor = 'white';
         erase.style.backgroundColor = 'white';
+        gray.style.backgroundColor = 'white';
     } else {
         randomC = 0;
-        earserSelected--;
+        graySelected = 0;
+        earserSelected++;
+        gray.style.backgroundColor = 'white';
         randomColor.style.backgroundColor = 'white';
         erase.style.backgroundColor = 'yellow';
     }
 })
 
+
+// reset game 
 reset.addEventListener('click', (e) => {
     resetall();
     e.stopPropagation()
@@ -78,18 +110,19 @@ canvas.addEventListener('mouseup', (e) => {
 
 });
 
-function createSquares(r, c) {
-    while(canvas.firstChild){
+function createSquares(r) {
+    while (canvas.firstChild) {
         canvas.removeChild(canvas.firstChild);
     }
-    for (let i = 0; i < r * c; i++) {
+    for (let i = 0; i < r * r; i++) {
         let div = document.createElement('div');
         div.classList.add('square');
         canvas.appendChild(div);
         div.setAttribute('id', 'Squares')
+        div.style.width = (canvasHeight / r).toString() + 'px';
+        div.style.height = (canvasHeight / r).toString() + 'px';
+        div.style.backgroundColor = '#FFFFFF';
     }
-    canvas.style.width = (r * 10).toString() + 'px';
-    canvas.style.height = (c * 10).toString() + 'px';
 }
 
 function coloring(e) {
@@ -98,12 +131,23 @@ function coloring(e) {
             let n = (Math.random() * 0xfffff * 1000000).toString(16);
             rand = '#' + n.slice(0, 6);
             e.target.style.backgroundColor = rand;
+        } else if (graySelected) {
+            makeMoreGray(e.target);
         } else if (earserSelected) {
             e.target.style.backgroundColor = 'white';
         } else {
-            e.target.style.backgroundColor = 'black';
+            e.target.style.backgroundColor = colorPicker.value;
         }
     }
+}
+
+
+function makeMoreGray(e) {
+    let n = Number(e.style.backgroundColor.replace(/\D/g,'')); 
+    let color_init = n % 1000;
+    gunit = color_init - 20; 
+    e.style.backgroundColor = 'rgb(' + gunit.toString() + ','+ gunit.toString() + ','+ gunit.toString() + ')';
+    console.log(e.style.backgroundColor);
 }
 
 function resetall() {
@@ -111,4 +155,8 @@ function resetall() {
     container.forEach(element => {
         element.style.backgroundColor = 'white';
     });
+}
+
+function RGBtoArray(){
+
 }
